5ec010c0c33cd2873fb44455efc24194e6f15c11
vunguyen12 <54542419+vunguyen12@users.noreply.github.com>
2019-09-09 09:50:22 -0500

    update

---
+++ src/DemoRaise.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
+public class DemoRaise {
+    public static void main(String[] args){
+        double salary;
+        pl("Demonstrating some raises:");
+         pl("enter your salary: ");
+
+        Scanner input = new Scanner(System.in);
+        salary = input.nextDouble();
+        predictRaise(salary);
+
+    }
+    public static void predictRaise(double salary)
+    {
+     double newSalary;
+     final double RAISE_RATE = 1.10;
+     newSalary = salary * RAISE_RATE;
+
+     pl("current salary: " + salary +"\nafter raise :" + newSalary);
+
+    }
+
+    public static void pl(String p) {
+        System.out.println(p);
+    }
+}
---
+++ src/example/Example1.java
@@ -0,0 +1,24 @@
+package example;
+
+public class Example1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+        displayMessage();
+        displayAddress();
+        displayAddress();
+
+
+
+    }
+    public static void displayMessage(){
+        displayAddress();
+        System.out.println("Hello World");
+
+    }
+        public static void  displayAddress()
+        {
+            System.out.println("3505 West Locust Street");
+            System.out.println("Davenport, Iowa 52804");
+        }
+}
